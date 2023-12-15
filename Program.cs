1a03c020599a331a1f319d6491f81a9d5f049573
unknown <davidoduntan1@gmail.com>
2023-12-15 17:43:23 +0100

    first code challenge commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-conso{le-template for more information
+PascalTriangle();
+
+
+
+static void PascalTriangle()
+{
+    var number = 5;
+    long c = 1;
+    for(int i = 0; i < number; i++)
+    {
+        c *= 11;
+    }
+    Console.Write(c); 
+}
+static void MajorityElement(int[] arr)
+{
+
+}
